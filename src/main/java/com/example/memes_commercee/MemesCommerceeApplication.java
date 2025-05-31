99f0e9a0cb3eb20284c45f6a6a556ae95a305b8a
mihirb1234 <mihirbhalerao7@gmail.com>
2025-05-31 18:47:20 +0530

    first commit

---
+++ src/main/java/com/example/memes_commercee/MemesCommerceeApplication.java
@@ -0,0 +1,13 @@
+package com.example.memes_commercee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemesCommerceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemesCommerceeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/memes_commercee/MemesCommerceeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memes_commercee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemesCommerceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
